3e886627ca5c5c864545481daf9c1e84dd07e4fb
Flavia <flavia01ns@gmail.com>
2024-09-14 11:50:38 -0300

    Adiciona a permanência de dados ao ocorrer erro na senha

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 
-$Nome = $Email = $senha = $confSenha = $Faculdade = $Curso = $Periodo = '';
-$NomeErro = $EmailErro = $SenhaErro = $confSenhaErro = $MensagemErro = '';
+$Nome = $Email = $Senha = $confSenha = $Faculdade = $Curso = $Periodo = '';
+$NomeErro = $EmailErro = $SenhaErro = $confSenhaErro = '';
 
 
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         $Nome = $_POST['nome'];
         $Email = $_POST['email'];
         $Senha = $_POST['senha'];
-        $confSenha = $_POST['confsenha'];
+        $confSenha = $_POST['confSenha'];
         $Faculdade = $_POST['faculdade'];
         $Curso = $_POST['curso'];
         $Periodo = $_POST['periodo'];
@@ -16,8 +16,8 @@ $NomeErro = $EmailErro = $SenhaErro = $confSenhaErro = $MensagemErro = '';
         if (empty($Nome)) {
             $NomeErro = 'Insira seu nome.';
         }
-        if (empty($Email)) {
-            $EmailErro = 'Insira seu email.';
+        if (!filter_var($Email, FILTER_VALIDATE_EMAIL) || !str_ends_with($Email, '@ufrpe.br')){
+            $EmailErro = 'Insira um email com domínio @ufrpe.br';
         }
         if (empty($Senha)) {
             $SenhaErro = 'Insira sua senha.';
@@ -32,24 +32,24 @@ if (empty($NomeErro) && empty($EmailErro) && empty($SenhaErro) && empty($confSen
     $host = 'localhost';
     $banco = 'campuslink';
     $user = 'root';
-    $senha_banco = '';
+    $senha_user = '';
 
-    $con = mysqli_connect($host, $user, $senha_banco, $banco);
+    $con = mysqli_connect($host, $user, $senha_user, $banco);
 
     if(!$con){
         die('Erro na conexão' . mysqli_connect_error());
     }
 
-    $sql = "INSERT INTO usuarios(Nome, Email, Senha, Faculdade, Curso, Periodo) VALUES('$Nome', '$Email', '$Senha', '$Faculdade', '$Curso', '$Periodo')";
+    $smt = $con-> prepare ("INSERT INTO usuarios (Nome, Email, Senha, Faculdade, Curso, Periodo) VALUES (?, ?, ?, ?, ?, ?)");
+    $smt->bind_param('ssssss' , $Nome, $Email, $Senha, $Faculdade, $Curso, $Periodo);
 
-    $rs = mysqli_query($con, $sql);
-
-    if($rs){
+    if ($smt->execute()) {
         echo"Cadastro realizado com sucesso.";
     }else{ 
-        echo"Erro ao realizar o cadastro";
+        echo"Erro ao realizar o cadastro" . $smt->error; 
     }
+    
+    $smt->close();
     mysqli_close($con);
 }
-    }
-
+}
